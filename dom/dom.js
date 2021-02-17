e022cc244268013ebc9a60bc9def886ae94059f6
Habibi Farid <FaridTH@code.local>
2021-02-17 15:54:54 +0400

    added jquery

--- dom/dom.js
+++ dom/dom.js
@@ -1,10 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $(document).on("click", ".card-header", function() {
+    $(this)
+      .next()
+      .slideToggle();
+  });
 
-$(document).on("click",".card-header",function(){
-$(this).next().slideToggle();
-
-})
-
-
-
-})
+  $(".card-header").on("click", function() {
+    if (
+      $(this)
+        .children(".minus")
+        .hasClass("hidden")
+    ) {
+      $(this)
+        .children(".minus")
+        .removeClass("hidden");
+      $(this)
+        .children("i")
+        .addClass("hidden");
+    } else {
+      $(this)
+        .children(".minus")
+        .addClass("hidden");
+      $(this)
+        .children("i")
+        .removeClass("hidden");
+    }
+  });
+  // $(".fas fa-plus").after("<h2>-</h2>");
+});
